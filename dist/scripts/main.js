73e1fa526ea6c3f5c3c3b2de52b7d259f2e318a0
Kristen Rieger <karieger1@gmail.com>
2015-06-02 22:06:56 -0500

    styling done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -2,65 +2,72 @@ $(document).ready(function() {
 
 	var App = Backbone.Router.extend({
 
-	routes: {
-		"":                 	"home",  
-		"home": 				"home",  
-		"search/:query":        "search",  
-	},
-
-	home: function() {
-		$("#page").hide();
-		$(".search-form").show();
-		$(".search").hide();
-
-    	console.log("home");
-
-	},
-//home function referenced in routes object
-  	 search: function(query) {		
-	     $(".page").hide(); //whatever is inside quotes is a css selector
-	     $(".search-form").show();
-
-   //  	console.log("search", query);
-
-
-	function onReceivedMovies(movies) {
-		console.log(onReceivedMovies);
-		console.log(movies);
-
-		
-
-	}	//console.log(movies.Search[0].title);	
-  	//get list of movies
-	$.get(
-		//API endpoint - url where we are retreiving the info
-		'http://www.omdbapi.com/',
-		// parameters (defined on http://www.omdbapi.com/)
-		{
-			s: 'star wars',
-			type: ['series', 'movie']
+		routes: {
+			"":                 	"home",  
+			"search/:query":        "search",  
 		},
-		// function to call when data is received
-		onReceivedMovies,
-		// format that we are expecting
-		'json'
-	)
-	
-  }
-});
+
+		home: function() {
+			$(".page").hide();
+			$("#home").show();
+			
+		},
+
+	//home function referenced in routes object
+	  	 search: function(query) {		
+		     $(".page").hide(); //whatever is inside quotes is a css selector
+		     $(".search-form").show();
+
+	   //  	console.log("search", query);
+	   		}
+	   	}); //close routing section -- all set
+
+
+	// function onReceivedMovies(movies) {
+	// 	console.log(onReceivedMovies);
+	// 	console.log(movies);
+
+	// }	//console.log(movies.Search[0].title);	
+  	//get list of movies
 
 	var myRouter = new App();
 	Backbone.history.start();
 
-	$("#search-form").on("submit", function(e){
+	$("#search-form").on("submit", function(e) {
 		e.preventDefault(); //must have this!
 
-		var query = $("#query").val();
-
+		var query = $("#film-query").val();
 		myRouter.navigate("search/"+ query, {trigger: true}); //trigger says after you change URL, change page too
 
-		console.log(query);
+		function onReceivedMovies(movies) {
 
+			var movieString = " ";
 
-  });
+			for(var i = 0; i < movies.Search.length; i++) {
+				var searchResults = movies.Search;
+				var allMovies = searchResults[i].Title;
+
+				console.log(allMovies);
+
+				movieString += "<div>" + allMovies + "</div>";
+
+			}
+
+			$("#filmList").html(movieString);
+  		}
+
+  		$.get(
+		//API endpoint - url where we are retreiving the info
+		'http://www.omdbapi.com/',
+		// parameters (defined on http://www.omdbapi.com/)
+		{
+			s: "query"
+			
+		},
+		// function to call when data is received
+		onReceivedMovies,
+		// format that we are expecting
+		"json"
+		);
+	});
 });
